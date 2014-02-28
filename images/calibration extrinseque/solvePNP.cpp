53964e523a0128d915c4bbbcf2d2b91044515fce
Theo Segonds <theo.segonds@gmail.com>
2014-02-28 02:22:02 +0100

    ajout inversion

--- images/calibration extrinseque/solvePNP.cpp
+++ images/calibration extrinseque/solvePNP.cpp
@@ -45,20 +45,48 @@ int main( int argc, char* argv[])
  std::cout << "Initial cameraMatrix: " << cameraMatrix << std::endl;
   Mat rvec(3,1,DataType<double>::type);
   Mat tvec(3,1,DataType<double>::type);
+  cv::Mat rotationMatrix(3,3,cv::DataType<double>::type);
  
   solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs, rvec, tvec);
+  cv::Rodrigues(rvec,rotationMatrix);
  
   std::cout << "rvec: " << rvec << std::endl;
   std::cout << "tvec: " << tvec << std::endl;
+  std::cout << "rotationMatrix: " << rotationMatrix << std::endl;
  
   //Verification de la projection
   std::vector<Point2f> projectedPoints;
-  projectPoints(objectPoints, rvec, tvec, cameraMatrix, distCoeffs, projectedPoints);
+  cv::projectPoints(objectPoints, rvec, tvec, cameraMatrix, distCoeffs, projectedPoints);
  
   for(unsigned int i = 0; i < projectedPoints.size(); ++i)
     {
     std::cout << "Image point: " << imagePoints[i] << " Projected to " << projectedPoints[i] << std::endl;
     }
+    
+   for(unsigned int i = 0; i < imagePoints.size(); ++i)
+     {
+		 cout<<imagePoints[i][1] ;
+   cv::Mat uvPoint = cv::Mat::ones(3,1,cv::DataType<double>::type); //u,v,1
+	uvPoint.at<double>(0,0) = 883.; //got this point using mouse callback
+	uvPoint.at<double>(1,0) = 202.;
+	
+	cv::Mat tempMat, tempMat2;
+double s;
+tempMat = rotationMatrix.inv() * cameraMatrix.inv() * uvPoint;
+tempMat2 = rotationMatrix.inv() * tvec;
+s = 0 + tempMat2.at<double>(2,0); //285 represents the height Zconst
+s /= tempMat.at<double>(2,0);
+std::cout << "P = " << rotationMatrix.inv() * (s * cameraMatrix.inv() * uvPoint - tvec) << std::endl ;
+
+   }
+
+    
+  //  rvec: [-0.6516997555859243; -0.6977915209735557; -0.9088726124391173]
+//tvec: [-0.5458128309114365; -0.2893036301628076; 1.910207684906602]
+/*finalm = [0.4332427076264066, 0.8638966371873064, -0.2568722572505709, -0.5458128309114365;
+  -0.4712986590380123, 0.4600923336570399, 0.7524577187450405, -0.2893036301628076;
+  0.7682306491396391, -0.2049332690572291, 0.6064849750455537, 1.910207684906602]
+*/
  
   return 0;
 }
@@ -103,13 +131,13 @@ std::vector<Point3f> Generate3DPoints()
   x=0;y=0;
   points.push_back(Point3f(x,y,z));
  
-  x=1;y=0;
+  x=150;y=0;
   points.push_back(Point3f(x,y,z));
  
-  x=1;y=1;
+  x=150;y=150;
   points.push_back(Point3f(x,y,z));
  
-  x=0;y=1;
+  x=0;y=150;
   points.push_back(Point3f(x,y,z));
  
  
